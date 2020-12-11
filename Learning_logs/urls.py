8afad19e6a2423d8fd19c0f34246fbf882f1c452
SymOwen <sym.owen@gmail.com>
2020-12-12 00:57:51 +1100

    finish chapter 18

--- Learning_logs/urls.py
+++ Learning_logs/urls.py
@@ -1,10 +1,12 @@
 """Defines URL patterns for Learning_logs."""
 
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    re_path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
